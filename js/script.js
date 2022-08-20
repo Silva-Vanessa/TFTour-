9cf21c87c6fb9046f152ad3017f908fc9fd8b815
Vanessa Silva <vanessaprogramacao@gmail.com>
2022-08-19 22:03:58 -0300

    Menu adicionado (carrossel desatualizado)

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let menu = document.getElementById("menu");
+
+function mostrarMenu() {
+    if(menu.style.display != "flex") {
+        menu.style.display = "flex"
+    }else{
+        menu.style.display = "none"
+    } 
+}
+
+
