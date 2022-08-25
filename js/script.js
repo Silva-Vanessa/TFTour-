ab999aa05446332ccb332e8fb6b84a0549387252
Vanessa Silva <vanessaprogramacao@gmail.com>
2022-08-24 21:27:58 -0300

    estrutura completa, mas sem o JavaScript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-let menu = document.getElementById("menu");
+$(document).ready(function(){
+    $("#barras").click(function(){
+        if($("menu").hasClass("menu-ativo")){
+            $("menu").removeClass("menu-ativo")
+        }else{
+            $("menu").addClass("menu-ativo")
+        }
+    })
+
+})
+
+/*let menu = document.getElementById("menu");
 
 function mostrarMenu() {
     if(menu.style.display != "flex") {
@@ -6,6 +17,81 @@ function mostrarMenu() {
     }else{
         menu.style.display = "none"
     } 
+}*/
+
+
+let email = document.getElementById("campo-email");
+
+function enviarEmail(){
+    let emailDigitado = email.value;
+    console.log(emailDigitado)
 }
 
+let listaNoticias = [
+    {
+        titulo: "Olá",
+        descricao: "Tudo bem?"
+    },
+    {
+        titulo: "Olá",
+        descricao: "Tudo bem?"
+    },
+    {
+        titulo: "Olá",
+        descricao: "Tudo bem?"
+    }
+    
+    ]
+
+function renderizarNoticias(){
+    let espaco = document.getElementById("espaco-noticias")
+
+    let template = "";
+
+    for (let index = 0; index < listaNoticias.length; index++) {
+        const element = listaNoticias[index];
+
+        template += `<div class="cardnews">
+        <img src="Img/noticias.png" alt="cardnew">
+        <h3>${noticia.titulo}</h3>
+        <p>${noticia.descricao}</p>
+      </div>`
+    }
+    espaco.innerHTML = template;
+}
+
+
+
+let listaVods = [
+    {
+        titulo: "Olá",
+        descricao: "Tudo bem?"
+    },
+    {
+        titulo: "Olá",
+        descricao: "Tudo bem?"
+    },
+    {
+        titulo: "Olá",
+        descricao: "Tudo bem?"
+    }
+    
+    ]
+
+function renderizarNoticias(){
+    let espacoV = document.getElementById("espaco-vods")
+
+    let template = "";
+
+    for (let index = 0; index < listaVods.length; index++) {
+        const element = listaVods[index];
+
+        template += ` <div class="cardnews">
+        <img src="Img/noticias.png" alt="cardnew">
+        <h3>${noticia.titulo}</h3>
+        <p>${noticia.descricao}</p>
+      </div>`
+    }
+    espacoV.innerHTML = template;
+}
 
